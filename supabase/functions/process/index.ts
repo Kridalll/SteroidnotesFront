82eb6ea314e2546f002412384558073d5890998d
Vic Khov <victorkhovtko@gmail.com>
2023-12-26 14:30:25 +0300

    Edge funcs: authorization check and document unloading from doc store (DB)

--- supabase/functions/process/index.ts
+++ supabase/functions/process/index.ts
@@ -1,23 +1,61 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { createClient } from '@supabase/supabase-js';
+import { processMarkdown } from '../_lib/markdown-parser.ts';
 
-console.log("Hello from Functions!")
+// These are automatically injected
+const supabaseUrl = Deno.env.get('SUPABASE_URL');
+const supabaseAnonKey = Deno.env.get('SUPABASE_ANON_KEY');
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+  if (!supabaseUrl || !supabaseAnonKey) {
+    return new Response(
+      JSON.stringify({
+        error: 'Missing environment variables.',
+      }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  const authorization = req.headers.get('Authorization');
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+  if (!authorization) {
+    return new Response(
+      JSON.stringify({ error: `No authorization header passed` }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+  const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    global: {
+      headers: {
+        authorization,
+      },
+    },
+    auth: {
+      persistSession: false,
+    },
+  });
+
+  const { document_id } = await req.json();
+
+  const { data: document } = await supabase
+    .from('documents_with_storage_path')
+    .select()
+    .eq('id', document_id)
+    .single();
+
+  if (!document?.storage_object_path) {
+    return new Response(
+      JSON.stringify({ error: 'Failed to find uploaded document' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+});
